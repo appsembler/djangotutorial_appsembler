d5697389a86000e66b06c37eca0c1ca4a3590c21
Nate Aune <natea@jazkarta.com>
2012-02-01 10:14:35 -0600

    Use our appsembler settings file instead of the app's file. This is used when serving the app in production via uWSGU.

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.appsembler_settings'
 
 import django.core.handlers.wsgi as w
 application = w.WSGIHandler()
